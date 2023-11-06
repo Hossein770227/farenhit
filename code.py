11572cb7b65447705d7f4d5d206d5f6cda62bc74
hossein <Hossein.m.7782@gmail.com>
2023-11-06 20:44:42 +0330

    fainally

--- code.py
+++ code.py
@@ -1,22 +1,40 @@
 import tkinter as tk 
 windoew = tk.Tk()
+
+
+etn_name = tk.Entry(
+    master=windoew,
+    width=30,
+)
+
+def show_resutl():
+    farenhit=etn_name.get()
+    try:
+        result=((int(farenhit)-32))* 5.9
+    except:
+        result=((float(farenhit)-32))* 5.9
+
+    lbl_result['text'] =result
+
+
 lbl_farenhit = tk.Label(
     master=windoew,
     text='farenhit : ',
-    height=5
+    height=5,
 )
-etn_name = tk.Entry(
-    master=windoew,
-    width=30
-)
+
+
+
 lbl_celicus = tk.Label(
     master=windoew,
     text='celicus',
 )
+
 btn_clc = tk.Button(
     master=windoew,
     text='calc',
-    width=5
+    command=show_resutl,
+    width=5,
 )
 lbl_result =tk.Label(
     master=windoew,
